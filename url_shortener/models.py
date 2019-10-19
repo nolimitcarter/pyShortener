2eac94bd8219c681f89612a24f79b8c47a417f3b
nolimitcarter <carterthayer@protonmail.com>
2019-10-19 15:22:37 -0400

    shortUrl generater and 404 page not found

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -1,3 +1,5 @@
+import string
+from random import choices
 from datetime import datetime
 
 from .extensions import db
@@ -9,4 +11,17 @@ class Link(db.Model):
   visits = db.Column(db.Interger, default=0)
   date_created = db.Column(db.DateTime, default=datetime.now)
 
-  
+  def __init__(self, **kwargs):
+    super().__init__(**kwards)
+    self.short_url = self.generate_short_link()
+
+  def generate_short_link(self):
+      characters = string.digit + string.ascii_letters
+      short_url = ''.join(choices(characters, k=3))
+
+      self.query.filter_by(short_url=short_url).first()
+
+      if link:
+        return self.generate_short_link()
+      
+      return short_url
