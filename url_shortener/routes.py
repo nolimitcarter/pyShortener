550956ca222285040867e4109462128f0fc989cc
nolimitcarter <carterthayer@protonmail.com>
2019-10-19 15:33:45 -0400

    up

--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 short = Blueprint('short', __name__)
 
@@ -8,13 +8,13 @@ def redirect_to_url(short_url):
 
 @short.route('/')
 def index():
-  pass
+  return render_template('index.html')
 
 @short.route('/add_link', methods=['POST'])
 def add_link():
   pass
 
-@short.stats('/stats')
+@short.route('/stats')
 def stats():
   pass
 
